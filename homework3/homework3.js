aacfff25c0953639976a83f7ba89da5465869113
jrestrad <jacob.estraa22@gmail.com>
2026-04-21 01:52:13 -0500

    Update homework3.js

--- homework3/homework3.js
+++ homework3/homework3.js
@@ -29,28 +29,55 @@ window.onload = function() {
     }
 }
  
-// check first name 
+// check first name regex helped with ai
 function checkFirstName() {
     var x = document.getElementById("firstName").value;
     var msg = document.getElementById("firstName_msg");
     if (x == "") {
         msg.innerHTML = "First name is required";
         msg.style.color = "red";
-    } else {
-        msg.innerHTML = "";
+        return false;
+       
+     if (!/^[A-Za-z'\- ]{1,30}$/.test(x)) {
+        msg.innerHTML = "Letters, apostrophes, dashes only";
+        msg.style.color = "red";
+        return false;
     }
+    msg.innerHTML = "";
+    return true;
 }
- 
-// check last name
+   //check mi 
+ function checkMI() {
+    var x = document.getElementById("middleInitial").value;
+    var msg = document.getElementById("middleInitial_msg");
+    if (x == "") {
+        msg.innerHTML = "";
+        return true;
+    }
+    if (!/^[A-Za-z]$/.test(x)) {
+        msg.innerHTML = "One letter only";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+// check last name regex helped with ai
 function checkLastName() {
     var x = document.getElementById("lastName").value;
     var msg = document.getElementById("lastName_msg");
     if (x == "") {
         msg.innerHTML = "Last name is required";
         msg.style.color = "red";
-    } else {
-        msg.innerHTML = "";
+        return false;
     }
+    if (!/^[A-Za-z'\- ]{2,30}$/.test(x)) {
+        msg.innerHTML = "2-30 letters, apostrophes, dashes only";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
 }
  
 // check dob
@@ -60,50 +87,235 @@ function checkDOB() {
     if (x == "") {
         msg.innerHTML = "Date of birth is required";
         msg.style.color = "red";
-    } else {
-        var dobDate = new Date(x);
-        var now = new Date();
-        var oldestOk = new Date();
-        oldestOk.setFullYear(now.getFullYear() - 120);
-        if (dobDate > now) {
-            msg.innerHTML = "Cannot be in the future";
-            msg.style.color = "red";
-        } else if (dobDate < oldestOk) {
-            msg.innerHTML = "Cannot be more than 120 years ago";
-            msg.style.color = "red";
-        } else {
-            msg.innerHTML = "";
-        }
+        return false;
     }
+    var dobDate = new Date(x);
+    var now = new Date();
+    var oldestOk = new Date();
+    oldestOk.setFullYear(now.getFullYear() - 120);
+    if (dobDate > now) {
+        msg.innerHTML = "Cannot be in the future";
+        msg.style.color = "red";
+        return false;
+    }
+    if (dobDate < oldestOk) {
+        msg.innerHTML = "Cannot be more than 120 years ago";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
 }
- 
-// check phone
+   //check email: regex helped with ai
+ function checkEmail() {
+    var box = document.getElementById("email");
+    box.value = box.value.toLowerCase();
+    var x = box.value;
+    var msg = document.getElementById("email_msg");
+    if (x == "") {
+        msg.innerHTML = "Email is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$/.test(x)) {
+        msg.innerHTML = "Format: name@domain.tld";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+// check phone regex helped with ai
 function checkPhone() {
     var x = document.getElementById("phone").value;
     var msg = document.getElementById("phone_msg");
     if (x == "") {
         msg.innerHTML = "";
-    } else if (!/^\d{3}-\d{3}-\d{4}$/.test(x)) {
+        return true;
+    }
+    if (!/^\d{3}-\d{3}-\d{4}$/.test(x)) {
         msg.innerHTML = "Format must be ###-###-####";
         msg.style.color = "red";
-    } else {
-        msg.innerHTML = "";
+        return false;
     }
+    msg.innerHTML = "";
+    return true;
 }
- 
+ function formatSSN() {
+    var box = document.getElementById("ssn");
+    var x = box.value.replace(/\D/g, "");
+    if (x.length > 9) { x = x.substring(0, 9); }
+    var out = x;
+    if (x.length > 5) {
+        out = x.substring(0, 3) + "-" + x.substring(3, 5) + "-" + x.substring(5);
+    } else if (x.length > 3) {
+        out = x.substring(0, 3) + "-" + x.substring(3);
+    }
+    box.value = out;
+}
+//ssn format and check regex helped with ai 
+function checkSSN() {
+    var x = document.getElementById("ssn").value;
+    var msg = document.getElementById("ssn_msg");
+    if (x == "") {
+        msg.innerHTML = "SSN is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/^\d{3}-\d{2}-\d{4}$/.test(x)) {
+        msg.innerHTML = "Must be 9 digits";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+//adress check 
+   function checkAddr1() {
+    var x = document.getElementById("addr1").value;
+    var msg = document.getElementById("addr1_msg");
+    if (x == "") {
+        msg.innerHTML = "Address is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (x.length < 2 || x.length > 30) {
+        msg.innerHTML = "Must be 2 to 30 characters";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+
+function checkAddr2() {
+    var x = document.getElementById("addr2").value;
+    var msg = document.getElementById("addr2_msg");
+    if (x == "") {
+        msg.innerHTML = "";
+        return true;
+    }
+    if (x.length < 2 || x.length > 30) {
+        msg.innerHTML = "Must be 2 to 30 characters";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+
+function checkCity() {
+    var x = document.getElementById("city").value;
+    var msg = document.getElementById("city_msg");
+    if (x == "") {
+        msg.innerHTML = "City is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/^[A-Za-z .'\-]{2,30}$/.test(x)) {
+        msg.innerHTML = "2-30 letters only";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+
+function checkState() {
+    var x = document.getElementById("state").value;
+    var msg = document.getElementById("state_msg");
+    if (x == "") {
+        msg.innerHTML = "Please select a state";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+
+function checkZip() {
+    var x = document.getElementById("zip").value;
+    var msg = document.getElementById("zip_msg");
+    if (x == "") {
+        msg.innerHTML = "ZIP is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/^\d{5}$/.test(x)) {
+        msg.innerHTML = "Must be 5 digits";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}  
+   //check user id regex helped with ai
+   function checkUserId() {
+    var x = document.getElementById("userId").value;
+    var msg = document.getElementById("userId_msg");
+    if (x == "") {
+        msg.innerHTML = "User ID is required";
+        msg.style.color = "red";
+        return false;
+    }
+    if (/^[0-9]/.test(x)) {
+        msg.innerHTML = "Cannot start with a number";
+        msg.style.color = "red";
+        return false;
+    }
+    if (x.length < 5 || x.length > 20) {
+        msg.innerHTML = "Must be 5 to 20 characters";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/^[A-Za-z][A-Za-z0-9_\-]*$/.test(x)) {
+        msg.innerHTML = "Letters, numbers, _ and - only";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "";
+    return true;
+}
+   
 // check password
 function checkPassword() {
     var x = document.getElementById("pwd").value;
+    var userId = document.getElementById("userId").value;
     var msg = document.getElementById("pwd_msg");
     if (x == "") {
         msg.innerHTML = "Password is required";
         msg.style.color = "red";
-    } else if (x.length < 8) {
-        msg.innerHTML = "At least 8 characters required";
-        msg.style.color = "red";
-    } else {
-        msg.innerHTML = "";
+        return false;
     }
+    if (x.length < 8) {
+        msg.innerHTML = "At least 8 characters";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/[A-Z]/.test(x)) {
+        msg.innerHTML = "Need at least 1 uppercase letter";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/[a-z]/.test(x)) {
+        msg.innerHTML = "Need at least 1 lowercase letter";
+        msg.style.color = "red";
+        return false;
+    }
+    if (!/[0-9]/.test(x)) {
+        msg.innerHTML = "Need at least 1 digit";
+        msg.style.color = "red";
+        return false;
+    }
+    if (userId != "" && x.toLowerCase() == userId.toLowerCase()) {
+        msg.innerHTML = "Password cannot equal your User ID";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "Password OK";
+    msg.style.color = "green";
+    checkPasswords();
+    return true;
 }
  
 // convert userid to lowercase
@@ -117,19 +329,51 @@ function checkPasswords() {
     var p1 = document.getElementById("pwd").value;
     var p2 = document.getElementById("pwd2").value;
     var msg = document.getElementById("pwd2_msg");
-    if (p2.length > 0) {
-        if (p1 != p2) {
-            msg.innerHTML = "Passwords do not match";
-            msg.style.color = "red";
-        } else {
-            msg.innerHTML = "Passwords match";
-            msg.style.color = "green";
-        }
-    } else {
+    if (p2 == "") {
         msg.innerHTML = "";
+        return false;
+    }
+    if (p1 != p2) {
+        msg.innerHTML = "Passwords do not match";
+        msg.style.color = "red";
+        return false;
+    }
+    msg.innerHTML = "Passwords match";
+    msg.style.color = "green";
+    return true;
+}
+ //validate 
+function validateAll() {
+    var errors = 0;
+    if (!checkFirstName()) { errors++; }
+    if (!checkMI()) { errors++; }
+    if (!checkLastName()) { errors++; }
+    if (!checkDOB()) { errors++; }
+    if (!checkEmail()) { errors++; }
+    if (!checkPhone()) { errors++; }
+    if (!checkSSN()) { errors++; }
+    if (!checkAddr1()) { errors++; }
+    if (!checkAddr2()) { errors++; }
+    if (!checkCity()) { errors++; }
+    if (!checkState()) { errors++; }
+    if (!checkZip()) { errors++; }
+    if (!checkUserId()) { errors++; }
+    if (!checkPassword()) { errors++; }
+    if (!checkPasswords()) { errors++; }
+ 
+    var status = document.getElementById("validateStatus");
+    var submitBtn = document.getElementById("submitBtn");
+    if (errors == 0) {
+        status.innerHTML = "All fields OK. You can submit now.";
+        status.style.color = "green";
+        submitBtn.style.display = "inline-block";
+    } else {
+        status.innerHTML = "Please fix " + errors + " error(s) above.";
+        status.style.color = "red";
+        submitBtn.style.display = "none";
     }
 }
- 
+   
 // opens popup 
 function reviewData() {
     var formcontents = document.getElementById("patientForm");
